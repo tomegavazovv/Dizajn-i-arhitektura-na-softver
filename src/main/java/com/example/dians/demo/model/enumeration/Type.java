72c2dff0a8183698935bfd6b49c265224f4aefe0
tomegavazovv <70688345+tomegavazovv@users.noreply.github.com>
2021-12-06 23:54:41 +0100

    src

---
+++ src/main/java/com/example/dians/demo/model/enumeration/Type.java
@@ -0,0 +1,10 @@
+package com.example.dians.demo.model.enumeration;
+
+public enum Type {
+    RESTAURANT,
+    PUB,
+    FASTFOOD,
+    BAR,
+    COFFEESHOP,
+    NIGHTCLUB
+}
---
+++ src/main/java/com/example/dians/demo/service/PlaceService.java
@@ -0,0 +1,9 @@
+package com.example.dians.demo.service;
+
+import com.example.dians.demo.model.Place;
+
+import java.util.List;
+
+public interface PlaceService {
+    List<Place> listAll();
+}
